df540917d35c8d80e11da6b032072628180aaf00
Cih <angel08012000@gmail.com>
2021-05-15 23:16:28 +0800

    前端base_flow.html
    把URL拉出來便全域變數

---
+++ static/assets/js/base_flow.js
@@ -0,0 +1,61 @@
+var head_url = "http://127.0.0.1:5000/";
+
+//start
+function start(){
+    console.log("start");
+    var myURL = head_url + "welcome";
+    console.log("myURL: "+myURL);
+    $.ajax({
+        url: myURL,
+        type: "GET",
+        dataType: "json",
+        contentType: 'application/json; charset=utf-8',
+        success: function(response){
+            console.log("response: "+response);
+            console.log(response.text);
+            document.getElementById("content").innerHTML += '<font color="black">'+response.text+'</font><br>';
+        },
+        error: function(){
+            console.log("error");
+        }
+    });
+
+    var myURL = head_url + "ask_os";
+    $.ajax({
+            url: myURL,
+            type: "GET",
+            dataType: "json",
+            contentType: 'application/json; charset=utf-8',
+            success: function(response){
+                console.log("response: "+response);
+                console.log(response.text);
+                document.getElementById("content").innerHTML += '<font color="black">'+response.text+'</font><br>';
+            },
+            error: function(){
+                console.log("error");
+            }
+        });
+}
+
+//send資料給rasa
+function send_to_bot(){
+    document.getElementById("content").innerHTML += '<font color="blue">'+$("#user_input").val()+'</font><br>';
+    var myURL = head_url + "base_flow_rasa?message="+$("#user_input").val();
+
+    console.log("myURL: "+myURL);
+    $.ajax({
+        url: myURL,
+        type: "GET",
+        dataType: "json",
+        contentType: 'application/json; charset=utf-8',
+        success: function(response){
+            console.log(response.text);
+            document.getElementById("content").innerHTML += '<font color="black">'+response.text+'</font><br>';
+        },
+        error: function(){
+            console.log("error");
+        }
+    });
+}
+
+window.addEventListener("load", start, false);
