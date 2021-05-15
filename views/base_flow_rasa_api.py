51acfdcb9ed5f0cebfb5c18f205bac3460f2cc51
Patty <L224749391@gmail.com>
2021-05-15 23:14:42 +0800

    delete translate

--- views/base_flow_rasa_api.py
+++ views/base_flow_rasa_api.py
@@ -18,8 +18,8 @@ base_flow_rasa_api=Blueprint('base_flow_rasa_api', __name__)
 @base_flow_rasa_api.route('base_flow_rasa')
 def base_flow_rasa():
     message=request.values.get('message')
-    translatedMessage=Translate(message).getTranslate()
-    print(translatedMessage)
+    #translatedMessage=Translate(message).getTranslate()
+    #print(translatedMessage)
     payload = {'sender': 'user', 'message': message}
     headers = {'content-type': 'application/json'}
     r = requests.post('http://localhost:5005/webhooks/rest/webhook', json=payload, headers=headers)
