02acdb0aee7cf286b30de759f59f63eb982a4f52
Patty <L224749391@gmail.com>
2021-08-01 18:44:31 +0800

    https

--- static/assets/js/common.js
+++ static/assets/js/common.js
@@ -1,5 +1,5 @@
 //var head_url = "http://0.0.0.0:55001/";
-var head_url = "http://140.121.197.130:55002/api/";
+var head_url = "https://140.121.197.130:55002/api/";
 
 function setPage(page){
     localStorage.setItem("page", page);
