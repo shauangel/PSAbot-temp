5a5ed13374a9fd10cb9b9c71023fe98136334390
Patty <L224749391@gmail.com>
2021-08-01 18:17:40 +0800

    proxy problem

--- static/assets/js/pages/menuBarJS/menuBar.js
+++ static/assets/js/pages/menuBarJS/menuBar.js
@@ -7,9 +7,9 @@ function changePage(){
     var page = localStorage.getItem("page");
     console.log(page);
     var content = "";
-    content += '<iframe MARGINWIDTH=0 MARGINHEIGHT=0 HSPACE=0 VSPACE=0 frameborder=0 scrolling=auto src="';
+    content += '<iframe MARGINWIDTH=0 MARGINHEIGHT=0 HSPACE=0 VSPACE=0 frameborder=0 scrolling=auto src="../../../../../templates/';
     content += page;
-//    content += '.html';
+    content += '.html';
     content += '" height="100%" width="100%"></iframe>';
     console.log("content: "+content);
     document.getElementById("main_page").innerHTML = content;
