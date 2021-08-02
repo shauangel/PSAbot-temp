c9157f1b82353d5959440bbd75febfd9834d5ae7
ditto129 <57188733+ditto129@users.noreply.github.com>
2021-08-02 12:16:59 +0800

    Update logout.js

--- static/assets/js/pages/loginJS/logout.js
+++ static/assets/js/pages/loginJS/logout.js
@@ -4,9 +4,15 @@
 <!-- Google Cloud Platform JS -->
 <script src="https://apis.google.com/js/platform.js?onload=onLoadGoogleCallback" async defer></script> 
 ============================================================== */
-function signOut() {
+function googleSignOut() {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
-        console.log('User signed out.');
+        console.log('Google User signed out.');
     });
+}
+
+function facebookLogout(){
+    FB.logout(function(response) {
+        console.log('Facebook User logout.');
+      });
 }
