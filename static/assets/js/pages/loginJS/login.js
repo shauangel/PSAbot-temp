115a784ed7594880538a04bebf69e598b39dfe50
ditto129 <57188733+ditto129@users.noreply.github.com>
2021-08-02 11:59:29 +0800

    login

--- static/assets/js/pages/loginJS/login.js
+++ static/assets/js/pages/loginJS/login.js
@@ -66,25 +66,30 @@ function onLoadGoogleCallback(){
     });
 
   auth2.attachClickHandler(element, {},
-    function(googleUser) {
-        var profile = googleUser.getBasicProfile();
-        $.ajax({
-          type: "POST",
-          url: head_url + 'google_sign_in',
-          data: JSON.stringify({
-            'id_token': googleUser.getAuthResponse().id_token
+    function(googleUser) 
+    {
+      var profile = googleUser.getBasicProfile();
+      $.ajax(
+      {
+        type: "POST",
+        url: head_url + 'google_sign_in',
+        data: JSON.stringify(
+          {
+          'id_token': googleUser.getAuthResponse().id_token
           }),
-          success: function () {
-            console.log('google login success')
-          },
-          dataType: 'application/json',
-          contentType: "application/json",
-        });
-        console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-        console.log('Name: ' + profile.getName());
-        console.log('Image URL: ' + profile.getImageUrl());
-        console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-      }, function(error) {
+        success: function () {
+          console.log('google login success')
+        },
+        dataType: 'application/json',
+        contentType: "application/json",
+      });
+      console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+      console.log('Name: ' + profile.getName());
+      console.log('Image URL: ' + profile.getImageUrl());
+      console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+      }, 
+      function(error) 
+      {
         console.log('Sign-in error', error);
       }
     );
