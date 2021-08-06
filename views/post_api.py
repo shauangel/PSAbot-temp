20662823543e1af35db43643b2253f5b06abc090
Angel Shau <shauangel2000@gmail.com>
2021-08-06 23:35:41 +0800

    update views api & models
    views/post_api -> textanalyzer function name correct
    views/TextAnalyze -> update to latest

--- views/post_api.py
+++ views/post_api.py
@@ -62,7 +62,7 @@ def insert_inner_post():
         }
         # 呼叫文字分析模組進行分析
         # textAnalyzer = TextAnalyze()
-        # post_dict['keyword'] = textAnalyzer.keywordExtration(post_dict['question'])
+        # post_dict['keyword'] = textAnalyzer.contentPreProcess(post_dict['question'])
         
         inner_post.insert_post(post_dict)
     except Exception as e :
@@ -85,7 +85,7 @@ def update_inner_post():
         }
         # 呼叫文字分析模組進行分析
         # textAnalyzer = TextAnalyze()
-        # post_dict['keyword'] = textAnalyzer.keywordExtration(post_dict['question'])
+        # post_dict['keyword'] = textAnalyzer.contentPreProcess(post_dict['question'])
         inner_post.update_post(post_dict)
     except Exception as e :
         post_dict = {"error" : e.__class__.__name__ + ":" +e.args[0]}
