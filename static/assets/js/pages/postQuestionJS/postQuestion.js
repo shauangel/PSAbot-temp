15040dc1ffd576346114e7bd9b1946f2bc8cb307
Cih <angel08012000@gmail.com>
2021-08-09 23:02:09 +0800

    debug textarea \n

--- static/assets/js/pages/postQuestionJS/postQuestion.js
+++ static/assets/js/pages/postQuestionJS/postQuestion.js
@@ -35,7 +35,7 @@ function showReplyContent(why){//why可以是see, save
                 
                 // 清除多餘的換行 START
                 while(true){
-                    if(userContent[i] == '<br>'){
+                    if(userContent[i] == '&#13;&#10;'){
                         i += 1;
                         console.log("i: "+i);
                     }
