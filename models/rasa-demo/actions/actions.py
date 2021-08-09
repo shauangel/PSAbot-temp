9eda8ab8a09999d9d801a13f7c7148a1d33e6700
Angel Shau <shauangel2000@gmail.com>
2021-08-09 21:53:56 +0800

    Update actions.py

--- models/rasa-demo/actions/actions.py
+++ models/rasa-demo/actions/actions.py
@@ -149,7 +149,10 @@ class analyze_and_search(Action):
             #取得block排名
             result = TextAnalyze.blockRanking(stack_items, qkey)
             #print(result)
-
+            for i in stack_items:
+                i['question']['abstract'] = textAnalyzer.textSummarization(i['question']['abstract'])
+                for ans in i['answers']:
+                    ans['abstract'] = textAnalyzer.textSummarization(ans['abstract'])
                     
             temp_data_id_list = requests.post(head_url + 'insert_cache', json={'data' : stack_items, 'type' : "temp_data"})
             block_rank_id = requests.post(head_url + 'insert_cache', json={'data': result, 'type' : "blocks_rank"})
