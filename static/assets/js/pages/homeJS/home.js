bc4e9f33e3914bec1da4973d0606a80574c7b9d3
Cih <angel08012000@gmail.com>
2021-08-09 17:28:22 +0800

    更新 home

--- static/assets/js/pages/homeJS/home.js
+++ static/assets/js/pages/homeJS/home.js
@@ -1,35 +1,89 @@
-function title(name, color) {
-  this.name = name;
-  this.color = color;
+function title(name, color, id) {
+    this.name = name;
+    this.color = color;
+    this.id = id;
 }
 
 // An array containing all the projects with their information
-var titles = [
-    new title('top 1 的標題', '#FFD700'),
-    new title('top 2 的標題', '#CDC9C9'),
-    new title('top 3 的標題', '#CD950C')
-];
+var titles = [];
 
-// Cacheing HTML elements
-var trophy = document.querySelector('#trophy');
-var questionName   = document.querySelector('#questionName');
-var titleDiv = document.querySelector('#titleDiv');
+var color = ['#FFD700', '#CDC9C9', '#CD950C'];
 
+function setLocalStorage(id){
+    localStorage.setItem("singlePostId", id);
+    localStorage.setItem("postType", "innerPost");
+    setPage('mySinglePostFrame');
+}
 
 function start(){
-    var index = 0;
-    setInterval(function(){
-        // Fade out
-        titleDiv.style.opacity = 0;
-        index += 1;
-        // Fade in 
-        setTimeout(function(){ 
-            questionName.innerHTML = titles[(index%3)].name;
-            trophy.style.color = titles[(index%3)].color;
-            trophy.class = "fa fa-trophy animate__swing";
-            titleDiv.style.opacity = 1;
-        },500);
-    }, 3000);
+    localStorage.removeItem("singlePostId");
+    var myURL = head_url + "query_hot_post";
+    $.ajax({
+        url: myURL,
+        type: "GET",
+        async: false, 
+        dataType: "json",
+        contentType: 'application/json; charset=utf-8',
+        success: function(response){
+            for(var i=0; i<response.hot_post.length; i++){
+                var getTitleURL = head_url+"query_inner_post";
+                var data = {_id: response.hot_post[i]}
+                $.ajax({
+                    url: getTitleURL,
+                    type: "POST",
+                    async: false, 
+                    data: JSON.stringify(data),
+                    dataType: "json",
+                    contentType: 'application/json; charset=utf-8',
+                    success: function(response){
+                        
+                        if(i>2){
+                            titles.push(new title(response.title, "#545058", response._id));
+                        }
+                        else{
+                            titles.push(new title(response.title, color[i], response._id));
+                        }
+                    },
+                    error: function(){
+                    }
+                });
+            }
+        },
+        error: function(){
+//            console.log("error");
+        }
+    });
+    
+    setTimeout(function(){
+        document.getElementById("titleDiv").innerHTML = '<i id="trophy" class="fa fa-code" aria-hidden="true" style="color: #545058; font-size: 25px; height: 5%; margin-top: 15px; margin-left: 15px; margin-right: 5px;"></i><span id="questionName">今日排行榜</span>';
+        
+        var index = -1;
+        // Cacheing HTML elements
+        var trophy = document.querySelector('#trophy');
+        var questionName   = document.querySelector('#questionName');
+        var titleDiv = document.querySelector('#titleDiv');
+        setInterval(function(){
+            // Fade out
+            titleDiv.style.opacity = 0;
+            
+            // Fade in 
+            setTimeout(function(){
+                index += 1;
+                questionName.innerHTML = titles[(index%(titles.length))].name;
+                trophy.style.color = titles[(index%titles.length)].color;
+                if((index%titles.length)>=3){
+                    trophy.className = "fa fa-code animate__swing";
+                }
+                else{
+                    trophy.className = "fa fa-trophy animate__swing";
+                }
+                titleDiv.onclick = function(){
+                    setLocalStorage(titles[(index%(titles.length))].id);
+                };
+                titleDiv.style.opacity = 1;
+            },500);
+        }, 3000);
+    }, 1000);
 }
 
 window.addEventListener("load", start, false);
