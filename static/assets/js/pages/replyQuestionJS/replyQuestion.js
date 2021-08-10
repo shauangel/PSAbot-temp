b60d986c78b490bab0e097d094bfb3954863d3a2
Cih <angel08012000@gmail.com>
2021-08-10 20:55:10 +0800

    熱門貼文數量

--- static/assets/js/pages/replyQuestionJS/replyQuestion.js
+++ static/assets/js/pages/replyQuestionJS/replyQuestion.js
@@ -205,12 +205,12 @@ function addInnerPostAnswer(){
             contentType: 'application/json; charset=utf-8',
             success: function(response){
     //            console.log("成功: 新增貼文通知（add_post_notification）");
-                setPage('mySinglePostFrame');
+//                setPage('mySinglePostFrame');
             },
             error: function(response){
     //            console.log("失敗: 新增貼文通知（add_post_notification）");
-    //            console.log(response);
-                window.alert("回覆貼文 失敗！\n請再試一次");
+                console.log(response);
+//                window.alert("回覆貼文 失敗！\n請再試一次");
             }
         });
 
