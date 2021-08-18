9650f55c5a087fab02f64d51a5d31bb749304858
ditto129 <57188733+ditto129@users.noreply.github.com>
2021-08-18 16:40:56 +0800

    google logout

--- static/assets/js/pages/menuBarJS/menuBar.js
+++ static/assets/js/pages/menuBarJS/menuBar.js
@@ -1051,27 +1051,21 @@ function save() {
 //////////////////儲存 照片＆姓名＆興趣標籤 END////////////////////
 
 //編輯個人資訊 END
-
-//////////// Google 登出需要 init gapi ////////////
-function onLoadGoogleCallback() {
-    gapi.load('auth2', function () {
-        // Retrieve the singleton for the GoogleAuth library and set up the client.
-        gapi.auth2.init({
-            client_id: '417777300686-b6isl0oe0orcju7p5u0cpdeo07hja9qs.apps.googleusercontent.com',
-            cookiepolicy: 'none',
-            scope: 'profile'
-        });
-        gauth = gapi.auth2.getAuthInstance();
-    });
-}
 ////////////////// 登出 START ////////////////////
 function logOut() {
     if (localStorage.getItem('role') == 'google_user') {
-        gauth = gapi.auth2.getAuthInstance();
-        gauth.signOut().then(function () {
-            gauth.disconnect();
-            console.log('Google User signed out.');
-        });
+        gapi.auth2.init(
+            {
+                client_id: '417777300686-b6isl0oe0orcju7p5u0cpdeo07hja9qs.apps.googleusercontent.com',
+                cookiepolicy: 'none',
+                scope: 'profile'
+            }
+        ).then(function () { 
+            var auth2 = gapi.auth2.getAuthInstance();
+            auth2.signOut().then(function () {
+                console.log('Google User signed out.');
+            });
+        })
     }
     else if (localStorage.getItem('role') == 'facebook_user') {
         FB.logout(function (response) {
