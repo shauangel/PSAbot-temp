76238d1a4a9793f5bc3365998432f9c9327a3fa7
Patty <L224749391@gmail.com>
2021-08-22 00:17:53 +0800

    Create socket_client.py

---
+++ socket_client.py
@@ -0,0 +1,25 @@
+import socket
+import sys
+import time
+
+socket_server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+socket_server.connect(("8.8.8.8", 80))
+ip = socket_server.getsockname()[0]
+sport = 8081
+
+print("This is your IP: ", ip)
+server_host = input("Enter friend\'s IP address:")
+name = input("Enter Friend\'s name:")
+
+socket_server.connect((server_host, sport))
+
+socket_server.send(name.encode())
+server_name = socket_server.recv(1024)
+server_name = server_name.decode()
+
+print(server_name, " has joined...")
+while True:
+    message = (socket_server.recv(1024)).decode()
+    print(server_name, ":", message)
+    message = input("Me : ")
+    socket_server.send(message.encode())
