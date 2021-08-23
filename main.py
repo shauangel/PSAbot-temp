511b885243f559876dec103d7ec547640a9c52be
ditto129 <57188733+ditto129@users.noreply.github.com>
2021-08-23 10:34:22 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,28 +5,15 @@ Created on Mon Mar 15 16:44:08 2021
 
 @author: linxiangling
 """
-
-#from flask import Flask
-#from flask import render_template
-
-#app = Flask(__name__)
-#@app.route("/")
-#def hello():
-#    return render_template('test.html')
-#if __name__ == "__main__":
-#    app.run(host="0.0.0.0", port=55001)
-
-from flask import Flask
+from flask import Flask ,Blueprint
 #from flask_apscheduler import APScheduler
 #from flask_security import Security
 from flask_cors import CORS
-#import models
 from views import register_blueprint
 #from lib import config
 from os import urandom
 from models.PSAbotLoginManager import PSAbotLoginManager,UserModel
 # --- encryption --- #
-from models.user import insert_user
 
 def create_app():
     app = Flask(__name__)
@@ -34,18 +21,10 @@ def create_app():
     app.config['SECRET_KEY'] = urandom(24).hex()
     #app.config.from_object(config.Config())
     CORS(app)
-    # models setup
-    #models.setup(app)
-
+    
     # security setup
-
-   # Security(app, models.user.USER_DATASTORE,login_form=models.user.ExtendedLoginForm)
-    ''' --- login manager ---- '''
-    login_manager = PSAbotLoginManager(app)
-    @login_manager.user_loader
-    def user_loader(user_id):  
-        user_now = UserModel(user_id)   
-        return user_now
+    # Security(app, models.user.USER_DATASTORE,login_form=models.user.ExtendedLoginForm)
+    
     # register app
     register_blueprint(app)
     return app
@@ -54,12 +33,22 @@ def create_app():
 #def refresh_schedule():
 #    models.reschedule.refresh_schedule()
 
+''' --- login manager ---- '''
+def login_manager_setup():
+    app = Blueprint('app', __name__)
+    login_manager = PSAbotLoginManager(app)
+    @login_manager.user_loader
+    def user_loader(user_id):  
+        user_now = UserModel(user_id)   
+        return user_now
+    
 
 
 
 if __name__ == "__main__":
     # scheduler=APScheduler()
     app = create_app()
+    login_manager_setup()
     # scheduler.init_app(app)
     # scheduler.start()
     app.run(host='0.0.0.0', port=55001)
