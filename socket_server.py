183764bed2f46c0b0f2af78681156cc71ad527c7
ditto129 <abc24511457@gmail.com>
2021-08-24 08:18:06 +0000

    conflict

--- socket_server.py
+++ socket_server.py
@@ -1,16 +1,9 @@
 import time, socket, sys
  
-<<<<<<< HEAD
 new_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host_name = socket.gethostname()
 s_ip = socket.gethostbyname(host_name)
 #s_ip = '127.0.0.1'
-=======
-new_socket = socket.socket(socket.AF_INET, socket.SOCKET_STREAM)
-host_name = socket.gethostname()
-#s_ip = socket.gethostbyname(host_name)
-s_ip = '8.8.8.8'
->>>>>>> bba6a35d21e83769ea8f811a2e9e987cabb55e1c
 port = 55003
 
 new_socket.bind((host_name, port))
