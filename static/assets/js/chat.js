973b7112b2d643103810213c676fbc633d489ff8
ditto129 <57188733+ditto129@users.noreply.github.com>
2021-08-25 18:12:08 +0800

    test socketio

--- static/assets/js/chat.js
+++ static/assets/js/chat.js
@@ -19,9 +19,12 @@ $(document).ready(function() {
   
 function join_room(){
     socket.emit('join_room' , {'id': socket.id});
-    socket.emit('room_msg' , {'id': socket.id,'msg':'hello~'});
 }
 
 function join_room2(){
     socket.emit('join_room2' , {'id': socket.id});
+}
+
+function say_hi(){
+    socket.emit('room_msg' , {'id': socket.id,'msg':'hello~'});
 }
