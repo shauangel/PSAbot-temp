dfa7265fc39bbb27ee8fa3bb61505d9da701f6fc
ditto129 <57188733+ditto129@users.noreply.github.com>
2021-08-25 14:58:15 +0800

    test socketio

--- psa_socket.py
+++ psa_socket.py
@@ -2,12 +2,13 @@ from flask_socketio import emit, join_room, leave_room
 from main import socketio
 
 
+print('----------socket-----------')
+@socketio.on('connect')
+def test_connect():
+    print('connected.')
+    emit('connect', 'server says connected.')
+
 @socketio.on('connect_event')
 def connected_msg(msg):
     print(msg)
-    emit('server_response', {'data': 'server received :' + msg['data']})
-
-# @socketio.on('client_event')
-# def client_msg(msg):
-    
-    
+    emit('server_response', 'received :' + str(msg))
