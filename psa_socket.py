113fe2eb3440bacf78f5e5d79c1e1ca8ce76913a
ditto129 <57188733+ditto129@users.noreply.github.com>
2021-08-26 16:01:06 +0800

    Update psa_socket.py

--- psa_socket.py
+++ psa_socket.py
@@ -1,40 +1,63 @@
 from flask_socketio import SocketIO,emit, join_room, leave_room
-from flask import jsonify
 from models import chat_data
-
-print('----------import psa_socket-----------')
+from datetime import datetime
 
 socketio = SocketIO()
 def init_socketio(app):
-    print('---------- init socket_io -----------')
-    socketio.init_app(app,cors_allowed_origins="*")
-    
+    print('# ---------- init socket_io ...')
+    socketio.init_app(app,cors_allowed_origins='*')
+# --------------- test socket.io --------------- #
 @socketio.on('connect')
 def test_connect():
-    print('---------- connected -----------')
+    print('# ---------- trigger connect event ...')
     emit('connect', 'server says connected.')
 
    
 @socketio.on('join_room')
 def join_chat_room(data):
-    print('---------- client join_room -----------')
+    print('# ---------- client emit join_room ...')
     print(data)
     join_room(data['room'])
     emit('room_msg', {'room': data['room'],'id':data['id'],'msg':'( has entered the room ... )'}, to=data['room'])
     
 @socketio.on('leave_room')
 def leave_chat_room(data):
-    print('---------- client leave_room -----------')
+    print('# ---------- client emit leave_room ...')
     print(data)
     leave_room(data['room'])
     emit('room_msg', {'room': data['room'],'id':data['id'],'msg':'( has left the room ... )'}, to=data['room'])
 
 @socketio.on('send_msg')
 def send_msg(data):
-    print('---------- client send_msg -----------')
+    print('# ---------- client emit send_msg ...')
     print(data)
     emit('room_msg', {'room': data['room'],'id':data['id'],'msg':data['msg']}, to=data['room'])
-
-
+    
+# ---------------------------------------------- #
+# 傳送問題資訊並建立聊天室
+@socketio.on('create_room')
+def create_room(data):
+    # data: {'question':'',tags:[],'asker':{'user_id':'','user_name':'','incognito':''}}
+    print('# ---------- client emit join_room ...')
+    print(data)
+    chat_dict = {
+        '_id':'',
+        'tags': data['tags'],
+        'question': data['question'],
+        'time':datetime.now().replace(microsecond=0),
+        'members': 
+        [
+            {
+            'user_id':data['asker']['user_id'],
+            'user_name':data['asker']['user_name'],
+            'incognito':data['asker']['incognito']
+            }
+        ],
+        'chat_logs':[]
+    }
+    chat_data.insert_chat(chat_dict)
+    # 將發問者加入聊天室
+    join_room(data['room'])
+    # emit('room_msg', {'room': data['room'],'id':data['id'],'msg':'( has entered the room ... )'}, to=data['room'])
     
     
