108ddf15cce14518e0db3d6460695df04eebaae5
ditto129 <57188733+ditto129@users.noreply.github.com>
2021-08-26 12:26:36 +0800

    test socket.io

--- static/assets/js/chat.js
+++ static/assets/js/chat.js
@@ -20,8 +20,7 @@ function set_room(){
     room_number = window.prompt('Room number');
     console.log('current room : ' + room_number);
     join_room(room_number);
-    document.getElementById("user_info").innerHTML = '<b>Socket id :</b> ' + socket.id + '<br>'
-                                                    + '<b>Room :</b> ' + room_number;
+    document.getElementById("user_info").innerHTML = '<b>Socket id :</b> ' + socket.id + ' <b>Room :</b> ' + room_number;
 }
 
 function change_room(){
