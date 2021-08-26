33765c2fd6de314ecfa8ff41773b0008735bddd1
ditto129 <57188733+ditto129@users.noreply.github.com>
2021-08-26 11:42:23 +0800

    test socket.io

--- psa_socket.py
+++ psa_socket.py
@@ -2,11 +2,11 @@ from flask_socketio import SocketIO,emit, join_room, leave_room
 from flask import jsonify
 
 
-print('----------import psa socket-----------')
+print('----------import psa_socket-----------')
 
 socketio = SocketIO()
 def init_socketio(app):
-    print('---------- init psa socket-----------')
+    print('---------- init socket_io -----------')
     socketio.init_app(app,cors_allowed_origins="*")
     
 @socketio.on('connect')
@@ -14,30 +14,29 @@ def test_connect():
     print('---------- connected -----------')
     emit('connect', 'server says connected.')
 
-@socketio.on('connect_event')
-def connected_msg(msg):
-    print('---------- connect_event -----------')
-    print(type(msg))
-    print(msg)
-    emit('server_response',msg)
-    
+   
 @socketio.on('join_room')
 def join_chat_room(data):
-    print('---------- join_room -----------')
+    print('---------- client join_room -----------')
+    print(data)
     print(data)
     join_room(data['room'])
-    emit('room_msg', data['id'] + ' has entered ' + data['room'], to=data['room'])
+    emit('room_msg', {
+        'msg':
+            '('+ data['room'] + ') ' + data['id'] + ' has entered room ' + data['room']}, to=data['room'])
     
 @socketio.on('leave_room')
 def leave_chat_room(data):
-    print('---------- leave_room -----------')
+    print('---------- client leave_room -----------')
     print(data)
     leave_room(data['room'])
-    emit('room_msg', data['id'] + ' has left ' + data['room'], to=data['room'])
+    emit('room_msg', {
+        'msg':
+            '('+ data['room'] + ') ' + data['id'] + ' has left room ' + data['room']}, to=data['room'])
 
 @socketio.on('send_msg')
 def send_msg(data):
-    print('---------- send_msg -----------')
+    print('---------- client send_msg -----------')
     print(data)
     emit('room_msg', '('+ data['room'] + ') ' + data['id'] + ' : ' + data['msg'], to=data['room'])
 
--- static/assets/js/chat.js
+++ static/assets/js/chat.js
@@ -1,36 +1,38 @@
 /* ============== socket.io測試 ============ */
 const socket = io('https://soselab.asuscomm.com:55002');
+var room_number;
 $(document).ready(function() {
-    console.log('socket : ' + Object.getOwnPropertyNames(socket))
     socket.on('connect', function(msg) {
-      console.log('socket.on : ' + msg);
-      socket.emit('connect_event', {data: 'js say connected!'});
+      console.log('connect response' + msg);
     })
-  
-    socket.on('server_response', function(msg) {
-        console.log('server_response : ' + typeof msg + ' ' + msg.data);
-     });
-  
-    socket.on('room_msg', function(msg) {
-      console.log('room_msg : ' + msg);
+
+    socket.on('room_msg', function(data) {
+      console.log('room_msg : ' + data);
+      document.getElementById("messages").innerHTML += '<li class="list-group-item">' + data['msg'] +'</li>';
+      $("#user_input").val("");
    });
-   
+
+   set_room();
 });
-  
-function join_room(){
-    socket.emit('join_room' , {'id': socket.id,'room':'room01'});
-    localStorage.setItem('room', 'room01');
+function set_room(){
+    room_number = window.prompt('Room number');
+    console.log('current room : ' + room_number);
+    join_room(room_number);
 }
 
-function join_room2(){
-    socket.emit('join_room' , {'id': socket.id,'room':'room02'});
-    localStorage.setItem('room', 'room02');
+function change_room(){
+    leave_room(room_number);
+    set_room();
 }
 
-function leave_room(){
-    socket.emit('leave_room' , {'id': socket.id,'room':localStorage.getItem('room')});
+function join_room(room){
+    socket.emit('join_room' , {'id': socket.id,'room':room});
 }
 
-function say_hi(){
-    socket.emit('send_msg' , {'id': socket.id,'room': localStorage.getItem('room'),'msg':'hello~'});
+function leave_room(room){
+    socket.emit('leave_room' , {'id': socket.id,'room':room});
+}
+
+function send_message(msg){
+    socket.emit('send_msg' , {'id': socket.id,'room': room_number,'msg':msg});
 }
