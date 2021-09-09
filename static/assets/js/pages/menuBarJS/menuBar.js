b2f44608b0f716a6f361703f47dac8d7a060bfde
Cih <angel08012000@gmail.com>
2021-09-10 01:14:45 +0800

    共同討論 - 前置

--- static/assets/js/pages/menuBarJS/menuBar.js
+++ static/assets/js/pages/menuBarJS/menuBar.js
@@ -24,15 +24,15 @@ var needDiscussQuestion = false;
 function bot(string) {
     console.log("bot的回覆: "+string);
     // 共同討論完，要重啟rasa
-    if(string=="請稍等，立即為你詢問其他使用者。"){
+    if(string==undefined){
+       bot("出現了點問題，請稍後再試～");
+    }
+    else if(string=="請稍等，立即為你詢問其他使用者。"){
         createDiscussRoom();
         setTimeout(welcomeAPI, 5000);//等一下再呼叫
     }
-    if(string.slice(0, 4)=="接收到了"){
+    else if(string.slice(0, 4)=="接收到了"){
         needDiscussQuestion = true;
-    }
-    if(string==undefined){
-       bot("出現了點問題，請稍後再試～");
     }
     //----- 處理選標籤 START -----//
     else if(string.slice(0,7)=="popover"){
