fb88e9c97fb1c2becd9ad02f82d43556b235f940
ditto129 <57188733+ditto129@users.noreply.github.com>
2021-09-21 10:04:41 +0800

    Update main.py

--- models/psa-socket/main.py
+++ models/psa-socket/main.py
@@ -74,9 +74,10 @@ def create_room(data):
             'type':'string',
             'content':'等待回答者加入...'
     }
-    join_message['time'] = str(join_message['time'])
-    emit('received_message', chat_dict, to=room_id)
     chat_data.insert_message(join_message)
+    join_message['time'] = str(join_message['time'])
+    emit('received_message', join_message, to=room_id)
+    
 
 # 使用者加入聊天室
 @socketio.on('join_room')
@@ -94,9 +95,9 @@ def join_chat_room(data):
             'type':'string',
             'content':'本次共同討論的問題是「 '+ question +'」，可以開始討論了。討論結束後請發問者輸入「結束討論」完成本次討論。'
     }
+    chat_data.insert_message(join_message)
     join_message['time'] = str(join_message['time'])
     emit('received_message', join_message, to=data['_id'])
-    chat_data.insert_message(join_message)
 
 @socketio.on('send_message')
 def send_message(data):
@@ -118,6 +119,22 @@ def send_message(data):
         # ------------------------------------------------- #
         end_sentences = ['結束討論','結束共同討論','完成討論']
         match = re.match(r'psabot ',chat_dict['content'],flags=re.IGNORECASE)
+        # 結束聊天狀態
+        if chat_data.end_chat(chat_dict['_id'],True,0):
+            payload = {'sender': chat_dict['user_id'],'message':chat_dict['content']}
+            headers = {'content-type': 'application/json'}
+            r = requests.post('http://localhost:5005/webhooks/rest/webhook', json=payload,headers=headers)
+            psa_message = {
+                        '_id':chat_dict['_id'],
+                        'user_id': 'PSAbot',
+                        'time': datetime.now().replace(microsecond=0),
+                        'type':'string',
+                        'content':r.json()[0]['text']
+                    }
+            if r.json()[0]['text'] != 'return_discussion':
+                chat_data.insert_message(psa_message)
+            psa_message['time'] = str(psa_message['time'])
+            emit('received_message', psa_message, to=chat_dict['_id'])
         if match != None and match.span()[0] == 0: # 若是psabot開頭，丟給faq_api
             payload = {'sender':data['user_id'],'message':(re.sub(r'psabot ','',chat_dict['content'],flags=re.IGNORECASE))}
             headers = {'content-type': 'application/json'}
@@ -159,24 +176,11 @@ def send_message(data):
                         'type':'string',
                         'content':r.json()[0]['text']
                     }
-                chat_data.insert_message(psa_message)
+                if r.json()[0]['text'] != 'return_discussion':
+                    chat_data.insert_message(psa_message)
                 psa_message['time'] = str(psa_message['time'])
                 emit('received_message', psa_message, to=chat_dict['_id'])
-        # 結束聊天狀態
-        if chat_data.end_chat(chat_dict['_id'],True,0):
-            payload = {'sender': chat_dict['user_id'],'message':chat_dict['content']}
-            headers = {'content-type': 'application/json'}
-            r = requests.post('http://localhost:5005/webhooks/rest/webhook', json=payload,headers=headers)
-            psa_message = {
-                        '_id':chat_dict['_id'],
-                        'user_id': 'PSAbot',
-                        'time': datetime.now().replace(microsecond=0),
-                        'type':'string',
-                        'content':r.json()[0]['text']
-                    }
-            chat_data.insert_message(psa_message)
-            psa_message['time'] = str(psa_message['time'])
-            emit('received_message', psa_message, to=chat_dict['_id'])
+        
         
     else:
         emit('received_message',
@@ -187,25 +191,6 @@ def send_message(data):
                  'type':'string',
                  'content':'Client isn\'t in room ' + data['_id'] + ', can\'t send messages.'},to=data['user_id'])
 
-# 關閉聊天室(不刪除紀錄)
-# @socketio.on('close_chat')
-# def close_chat(data):
-#     print('# ---------- client emit remove_chat ...')
-#     print(data)
-#     # 如果該room id有在client的room中
-#     if data['_id'] in rooms():   
-#         # data : { '_id','user_id','time','type','content'}
-#         close_room(data['_id'])
-#         emit('received_message','關閉' + data['_id'])
-#         chat_data.change_state(data['_id'],False)
-#     else:
-#         emit('received_message',
-#               {
-#                   '_id':data['user_id'],
-#                   'user_id':'system',
-#                   'time':str(datetime.now().replace(microsecond=0)),
-#                   'type':'string',
-#                   'content':'Client isn\'t in room ' + data['_id'] + ', can\'t close the chat.'},to=data['user_id'])
 
 # 取得聊天室歷史訊息
 @socketio.on('get_chat')
