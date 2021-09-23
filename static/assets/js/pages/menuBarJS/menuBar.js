64218afdbc25661dbd69f73e86495a4a16ddd2bc
Cih <angel08012000@gmail.com>
2021-09-23 17:29:16 +0800

    共同討論 - debug重複通知

--- static/assets/js/pages/menuBarJS/menuBar.js
+++ static/assets/js/pages/menuBarJS/menuBar.js
@@ -1577,7 +1577,7 @@ function received_message(){
                 disabledChatroom();
             }
         }
-        else if(check_discussion_is_full(response._id) == false){ // 代表是創房間
+        else if(response.user_id == null){ // 代表是創房間
             console.log("回覆的id: "+response._id);
             console.log("創建房間? "+check_discussion_is_full(response._id));
             discussRoomId = response._id;
