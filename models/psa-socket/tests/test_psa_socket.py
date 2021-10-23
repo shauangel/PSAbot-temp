ebac9c7482bb5f1b272c0ff7818392d59b7994d0
ditto129 <57188733+ditto129@users.noreply.github.com>
2021-10-23 23:24:13 +0800

    Update test_psa_socket.py

--- models/psa-socket/tests/test_psa_socket.py
+++ models/psa-socket/tests/test_psa_socket.py
@@ -20,5 +20,5 @@ class TestSocketIO(unittest.TestCase):
     def test_connect(self):
         client = socketio.test_client(app, query_string='?user_id=test_user')
         # client.get_received()[0]['args'] == 'test_user has connected.'
-        self.assertEqual(client.get_received()[0]['args'],'test_user has connected.')
+        self.assertEqual(client.get_received()[0]['args'][0],'test_user has connected.')
         
