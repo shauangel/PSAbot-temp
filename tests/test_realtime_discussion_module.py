c7efa24a6a8c3c56a99c7b7cd0bc10a844c5c207
ditto129 <57188733+ditto129@users.noreply.github.com>
2021-10-25 16:35:19 +0800

    test discuss

---
+++ tests/test_realtime_discussion_module.py
@@ -0,0 +1,11 @@
+from . import SettingBase
+from flask import url_for
+import json
+
+class CheckChatbotModule(SettingBase):
+    def discussion_recommand_user(self):
+        response = self.client.post(url_for('discussion_api.discussion_recommand_user') ,
+                                    data = json.dumps({"tags" : ["00000"]}), content_type="application/json")
+        with open('data/realtime-discussion-module/discussion_recommand_user_output.json', 'r', encoding = 'utf-8') as file:
+            output = json.load(file)
+        self.assertEqual(response.json,output)
