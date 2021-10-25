bf7abe2650eaa79b8a4e85efa9186478aa27879b
ditto129 <57188733+ditto129@users.noreply.github.com>
2021-10-25 22:21:56 +0800

    Update test_realtime_discussion_module.py

--- tests/test_realtime_discussion_module.py
+++ tests/test_realtime_discussion_module.py
@@ -3,7 +3,7 @@ from flask import url_for
 import json
 
 class CheckChatbotModule(SettingBase):
-    def discussion_recommand_user(self):
+    def test_discussion_recommand_user(self):
         response = self.client.post(url_for('discussion_api.discussion_recommand_user') ,
                                     data = json.dumps({"tags" : ["00000"]}), content_type="application/json")
         with open('data/realtime-discussion-module/discussion_recommand_user_output.json', 'r', encoding = 'utf-8') as file:
