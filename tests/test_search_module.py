be253aee3fea94e686a099f24800b9fde9600de1
ditto129 <57188733+ditto129@users.noreply.github.com>
2021-10-29 17:22:18 +0800

    Update test_search_module.py

--- tests/test_search_module.py
+++ tests/test_search_module.py
@@ -14,7 +14,7 @@ class CheckTextAnalyzeModule(SettingBase):
         self.assertEqual(response.json,output)
     
     def test_keywords(self):
-        response = self.client.get(url_for('rasa_api.keywords')+ "?sender_id=116287698501129823679&keywords=keywords_for_guided_QA,falsk",follow_redirects=True)
+        response = self.client.get(url_for('rasa_api.keywords')+ "?sender_id=116287698501129823679&keywords=keywords_for_guided_QA,flask",follow_redirects=True)
         with open('data/search-module/keywords_output.json', 'r', encoding = 'utf-8') as file:
             output = json.load(file)
         self.assertEqual(response.json,output)
@@ -30,19 +30,17 @@ class CheckTextAnalyzeModule(SettingBase):
         
     def test_query_hot_post(self):
         response = self.client.get(url_for('post_api.query_hot_post'),follow_redirects=True)
-        with open('data/tag-maintenance-module/query_hot_post_output.json', 'r', encoding = 'utf-8') as file:
+        with open('data/search-module/query_hot_post_output.json', 'r', encoding = 'utf-8') as file:
             output = json.load(file)
         self.assertEqual(response.json,output)
         
     def test_query_inner_search(self):
         with open('data/search-module/query_inner_search_input.json', 'r', encoding = 'utf-8') as file:
             text_input = json.load(file)
-        # with open('data/search-module/query_inner_search_output.json', 'r', encoding = 'utf-8') as file:
-        #     output = json.load(file)
+        with open('data/search-module/query_inner_search_output.json', 'r', encoding = 'utf-8') as file:
+            output = json.load(file)
         response = self.client.post(url_for('post_api.query_inner_search') ,
                                     data = json.dumps(text_input), content_type="application/json")
-        with open('data/search-module/query_inner_search_output.json', 'w', encoding = 'utf-8') as file:
-            json.dump(response.json,file)
-        self.assertEqual(response.json,response.json)
+        self.assertEqual(response.json,output)
         
     
