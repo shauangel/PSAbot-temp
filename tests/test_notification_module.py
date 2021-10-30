9be44ed065bff2449850a0b50e713b59ab652eed
ditto129 <57188733+ditto129@users.noreply.github.com>
2021-10-30 12:53:19 +0800

    Update test_notification_module.py

--- tests/test_notification_module.py
+++ tests/test_notification_module.py
@@ -1,10 +1,9 @@
 from . import SettingBase
 from flask import url_for
 import json
-import pytest
 
 class CheckNotificationModule(SettingBase):
-    @pytest.mark.order1
+    
     def test_check_notification_content(self):
         response = self.client.get(url_for('notification_api.check_notification_content') + 
                                    "?user_id=116287698501129823679&page=0",follow_redirects=True)
@@ -12,7 +11,7 @@ class CheckNotificationModule(SettingBase):
             output = json.load(file)
         self.assertEqual(response.json,output)
         
-    @pytest.mark.order2
+        
     def test_add_post_notification(self):
         response = self.client.get(url_for('notification_api.add_post_notification') +
                                    "?user_id=4028146397302662&replier_id=116287698501129823679&post_id=000015",follow_redirects=True)
@@ -20,7 +19,7 @@ class CheckNotificationModule(SettingBase):
             output = json.load(file)
         self.assertEqual(response.json,output)
         
-    @pytest.mark.order3   
+        
     def test_delete_notification(self):
         response = self.client.get(url_for('notification_api.delete_notification') +
                                    "?post_id=000059",follow_redirects=True)
@@ -28,7 +27,7 @@ class CheckNotificationModule(SettingBase):
             output = json.load(file)
         self.assertEqual(response.json,output)
         
-    @pytest.mark.order4
+        
     def test_add_discussion_invitation_notification(self):
         with open('data/notification-module/add_discussion_invitation_notification_input.json', 'r', encoding = 'utf-8') as file:
             text_input = json.load(file)
@@ -38,7 +37,7 @@ class CheckNotificationModule(SettingBase):
                                     data = json.dumps(text_input), content_type="application/json")
         self.assertEqual(response.json,output)
         
-    @pytest.mark.order5
+        
     def test_disable_discussion_invatation(self):
         response = self.client.get(url_for('notification_api.delete_notification') +
                                    "?user_id=116287698501129823679&id=45",follow_redirects=True)
@@ -46,7 +45,7 @@ class CheckNotificationModule(SettingBase):
             output = json.load(file)
         self.assertEqual(response.json,output)
         
-    @pytest.mark.order6 
+        
     def test_check_new_notification(self):
         response = self.client.get(url_for('notification_api.check_new_notification') +
                                    "?user_id=116287698501129823679",follow_redirects=True)
