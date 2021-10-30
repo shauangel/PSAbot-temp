2912c2b4d97f0893d5874ea828c1fa9bb7940793
ditto129 <57188733+ditto129@users.noreply.github.com>
2021-10-31 01:49:45 +0800

    test psa-socket

--- models/psa-socket/tests/test_psa_socket.py
+++ models/psa-socket/tests/test_psa_socket.py
@@ -5,24 +5,45 @@ from main import socketio,app
 def connect_client(user_id):
     client = socketio.test_client(app, query_string='?user_id=' + user_id)
     return client
-    
+
+def get_file_input(data_folder,function_name):
+    with open('data/' + data_folder + '/' + function_name + '_input.json', 'r', encoding = 'utf-8') as file:
+            text_input = json.load(file)
+    return text_input
+
+def get_file_output(data_folder,function_name):
+    with open('data/' + data_folder + '/' + function_name + '_output.json', 'r', encoding = 'utf-8') as file:
+            text_input = json.load(file)
+    return text_input
+
 class TestSocketIO(unittest.TestCase):
-    def connect_client(self,user_id):
-        client = socketio.test_client(app, query_string='?user_id=' + user_id)
-        return client
     def test_connect(self):
         client = socketio.test_client(app, query_string='?user_id=test_user')
         # client.get_received()[0]['args'] == 'test_user has connected.'
         self.assertEqual(client.get_received()[0]['args'][0],'test_user has connected.')
         
-    def test_emit(self):
-        client = connect_client('test_user2')
-        client.get_received()
-        client.emit('get_chat', {
-            '_id':"000001",
-            'user_id':'test_user2'
-            })
+        
+    def test_create_room(self):
+        client = connect_client('116287698501129823679')
+        json_input = get_file_input('realtime-discussion', 'create_room')
+        json_output = get_file_output('realtime-discussion', 'create_room')
+        client.emit('create_room', json_input)
         received = client.get_received()
         self.assertEqual(received[0]['name'], 'received_message')
-        # self.assertEqual(received[0]['name'], 'test_user2')
-        self.assertEqual(received[0]['args'][0], {"_id":"000001","tags":[{"tag_id":"00000","tag_name":"Python"}],"keywords":[],"question":"耶","time":{"$date":"2021-10-14T13:55:29.000Z"},"members":[{"user_id":"116287698501129823679","incognito":False},{"user_id":"4168365173258689","incognito":False}],"chat_logs":[{"user_id":"PSAbot","time":{"$date":"2021-10-14T13:55:29.000Z"},"type":"string","content":"等待回答者加入..."},{"user_id":"PSAbot","time":{"$date":"2021-10-14T13:57:44.000Z"},"type":"string","content":"本次共同討論的問題是「 耶」，可以開始討論了。討論結束後請發問者輸入「結束討論」完成本次討論。"},{"user_id":"116287698501129823679","time":{"$date":"2021-10-14T13:57:59.000Z"},"type":"string","content":"小小小可愛"},{"user_id":"4168365173258689","time":{"$date":"2021-10-14T13:58:05.000Z"},"type":"string","content":"小小小飛機"},{"user_id":"116287698501129823679","time":{"$date":"2021-10-14T13:58:10.000Z"},"type":"string","content":"開心開心"},{"user_id":"4168365173258689","time":{"$date":"2021-10-14T13:58:17.000Z"},"type":"string","content":"好累好累。"},{"user_id":"116287698501129823679","time":{"$date":"2021-10-14T13:58:26.000Z"},"type":"string","content":"結束討論"},{"user_id":"PSAbot","time":{"$date":"2021-10-14T13:58:26.000Z"},"type":"string","content":"請為「回答者」這次的回答評分（滿意/不滿意）。"},{"user_id":"116287698501129823679","time":{"$date":"2021-10-14T13:58:30.000Z"},"type":"string","content":"滿意"},{"user_id":"PSAbot","time":{"$date":"2021-10-14T13:58:31.000Z"},"type":"string","content":"請問你願意回報此問題嗎？（僅限提問者回覆）"},{"user_id":"116287698501129823679","time":{"$date":"2021-10-14T13:58:35.000Z"},"type":"string","content":"願意"}],"end_flag":True,"enabled":False})
+        self.assertEqual(received[0]['args'][0],json_output)
+        
+    def test_join_room(self):
+        client = connect_client('4168365173258689')
+        json_input = get_file_input('realtime-discussion', 'join_room')
+        client.emit('join_room', json_input)
+        received = client.get_received()
+        self.assertEqual(received[0]['name'], 'received_message')
+        
+    def test_send_message(self):
+        client = connect_client('116287698501129823679')
+        json_input = get_file_input('realtime-discussion', 'send_message')
+        json_output = get_file_output('realtime-discussion', 'send_message')
+        client.emit('send_message', json_input)
+        received = client.get_received()
+        self.assertEqual(received[0]['name'], 'received_message')
+        self.assertEqual(received[0]['args'][0],json_output)
