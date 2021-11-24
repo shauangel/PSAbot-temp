9e877e95593c11c52f0573b4b77eb983b9e13cb1
Cih <angel08012000@gmail.com>
2021-11-24 22:59:10 +0800

    測試

--- static/assets/js/pages/menuBarJS/menuBar.js
+++ static/assets/js/pages/menuBarJS/menuBar.js
@@ -1728,7 +1728,7 @@ function received_message(){
                    // PSAbot 說話
                     ImgYou = "../static/images/iconSmall.png";
                 }
-                else if(check_member_is_incognito(response._id, response.user_id)){
+                else if(check_member_is_incognito(response._id, response.user_id)==true){
                     console.log("共同討論 - 別人說話但匿名");
                     // 別人但匿名
                     ImgYou = "../static/images/discussionImg.png";
@@ -1738,6 +1738,10 @@ function received_message(){
                     // 別人沒匿名
                     ImgYou = getChatroomUserImg(response.user_id);
                 }
+                console.log("比對的部分: ");
+                console.log(check_member_is_incognito(response._id, response.user_id)==true);
+                console.log(check_member_is_incognito(response._id, response.user_id)=="true");
+                console.log(typeof(check_member_is_incognito(response._id, response.user_id)));
                 bot(response.content);
             }
             else if(response._id == userSessionId){
